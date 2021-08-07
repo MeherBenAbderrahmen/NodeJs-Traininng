a576e30a7952335143e0066f1af2e030928aa700
Ben Abderrahmen Meher <meherbenabderrahmen@gmail.com>
2021-08-07 12:51:16 +0200

    add API root

--- index.js
+++ index.js
@@ -11,10 +11,26 @@ app.get('/', (req, res) => {
 })
 
 //get all user
+app.get('/users',(req, res) => {
+  res.json({message:'user'});
+})
 //get one use by id
+app.get('/user/:id', (req, res) => {
+  res.json({message:'get one user by id'});
+})
 // add one user
+app.post('/users', (req, res) => {
+  res.json({message:'user add'});
+})
 //update user by id
+app.put('/user/:id', (req, res) => {
+  res.json({message:'user updated'});
+})
 // delete user by id
+app.delete('/user/:id', (req, res) => {
+  res.json({message:'user removed'});
+})
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`)
 })
