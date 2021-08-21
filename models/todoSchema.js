5bc2eaa8ed019b6418380d41557672c19ef01da6
Ben Abderrahmen Meher <meherbenabderrahmen@gmail.com>
2021-08-21 02:21:46 +0200

    create relation with populate

--- models/todoSchema.js
+++ models/todoSchema.js
@@ -2,7 +2,8 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 const todoSchema = new Schema({
   name: { type: String, required: true },
-  description: String
+  description: String,
+  
   },{
   versionKey:false,
   timestamps: true
