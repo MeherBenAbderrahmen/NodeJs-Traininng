4ad84974b9f5352599e4840f508787addd4ea195
Ben Abderrahmen Meher <meherbenabderrahmen@gmail.com>
2021-08-21 17:20:03 +0200

    multiple UploadFile

--- routes/uploadAPI.js
+++ routes/uploadAPI.js
@@ -13,15 +13,23 @@ const myStorage = multer.diskStorage({
         cb(null, newFileName)
     }
 });
-const fileFilterFunction = (req, file, cb) =>{
-const fileExtension = path.extname(file.originalname);
-const allowedExtension = ['.jpg','.png','.gif','.jpeg'];
-cb(null, allowedExtension.includes(fileExtension));
+const fileFilterFunction = (req, file, cb) => {
+    const fileExtension = path.extname(file.originalname);
+    const allowedExtension = ['.jpg', '.png', '.gif', '.jpeg', '.mp4'];
+    cb(null, allowedExtension.includes(fileExtension));
 }
-const maxSize = 1*1024*1024;
-const upload = multer({ storage: myStorage, fileFilter: fileFilterFunction, limits: {fileSize: maxSize}});
-router.post('/upload', upload.single('file'),  (req, res) => {
+const maxSize = 1 * 1024 * 1024;
+const upload = multer({ storage: myStorage, fileFilter: fileFilterFunction, limits: { fileSize: maxSize } });
+router.post('/upload', upload.single('file'), (req, res) => {
     if ((req.file) !== undefined) {
+        res.send({ message: 'file uploaded ' })
+    } else {
+        res.status(400).json({ message: 'File not uploaded' });
+    }
+});
+
+router.post('/uploadMultiple', upload.array('file', 3), (req, res) => {
+    if ((req.files.length) !== 0) {
         res.send({ message: 'file uploaded ' })
     } else {
         res.status(400).json({ message: 'File not uploaded' });
